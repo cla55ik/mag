00cacfc5056bb21348bb21505176f5d28841e644
Ivan <ivan5420@yandex.ru>
2021-06-21 10:22:42 +0300

    change do,main name

---
+++ layouts/head_empty.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="ru">
+  <head>
+    <!-- Обязательные метатеги -->
+    <meta charset="utf-8">
+    <!-- Настройка viewport -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Подключаем Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <!-- Подключаем Custom CSS -->
+    <link rel="stylesheet" type="text/css" href="/resources/css/style.css">
+    <!--Подключаем Шрифт-->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@100;200;300;500;600;700;800;900&display=swap" rel="stylesheet">
+    <!--Подключаем OWL corousel-->
+    <link rel="stylesheet" href="/resources/owlcarousel/owl.carousel.min.css">
+    <link rel="stylesheet" href="/resources/owlcarousel/owl.theme.default.min.css">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <script src="/resources/owlcarousel/owl.carousel.js"></script>
+    <script src="/resources/js/main.js"></script>
+
+    <!-- favicon -->
+    <link type="image/x-icon" rel="shortcut icon" href="/resources/img/favicon.svg">
+    <title><?=$page_title;?></title>
+
+  
+
+
+  </head>
+
+  <body>
+
+  <div class="row">
+    FDA
+  </div>
